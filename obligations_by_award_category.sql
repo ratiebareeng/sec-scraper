6b543bcccf3b6013cf1609f1b02b2bcd4cf3bd32
EM1989 <167819946+EM1989@users.noreply.github.com>
2024-09-02 13:23:01 -0400

    Deleted dupes

--- obligations_by_award_category.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "obligations_by_award_category" (
-  "agency_toptier_code" text,
-  "total_aggregated_amount" double precision
-);
-
-INSERT INTO "obligations_by_ award_category" ("agency_toptier_code","total_aggregated_amount")
-VALUES
-('097',206869322019.26);
